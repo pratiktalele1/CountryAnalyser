4d8313c37dc11eb4dc99bf69137f292c0a8d07f7
pratiktalele1 <pratiktalele1@gmail.com>
2021-08-12 17:28:10 +0530

    commiting Start project

---
+++ src/Classes/AnalyserMain.java
@@ -0,0 +1,10 @@
+package Classes;
+
+import java.io.IOException;
+
+public class AnalyserMain {
+	public static void main(String[] args) throws IOException {
+		UC1 uc1=new UC1();
+		uc1.readFile();
+	}
+}
---
+++ src/Classes/UC1.java
@@ -0,0 +1,24 @@
+package Classes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class UC1 {
+	File file=new File("countries of the world.csv");
+	public void readFile() throws FileNotFoundException {
+		Scanner scanner=new Scanner(file);
+		while(scanner.hasNext()) {
+			String countryDetails=scanner.nextLine();
+			if(countryDetails.contains("India")) {
+				System.out.println(countryDetails);
+			}
+		
+			
+		}
+	}
+	
+}
